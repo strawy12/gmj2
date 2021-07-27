eed6b1ba74bf334509d1b68aff6ca40b53976dce
minyoung529 <77655318+minyoung529@users.noreply.github.com>
2021-07-27 22:31:26 +0900

    스크립트 오류 수정

--- Assets/Scripts/MovingObstacle.cs
+++ Assets/Scripts/MovingObstacle.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class MovingObstacle : MonoBehaviour
 {
-<<<<<<< HEAD:Assets/MovingObstacle.cs
-    private float maxTime = 0.75f;
-=======
     [SerializeField]
     private float maxTime = 1f;
->>>>>>> 231bbbd79712acb9ba7b2bb41d3cb93c1cc4fd5c:Assets/Scripts/MovingObstacle.cs
     private float timer = 0f;
 
     private void Update()
